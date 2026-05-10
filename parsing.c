73c946750254bbd3a29ad85ac227ce7a7ee3216e
lena <lena.odum1@gmail.com>
2026-05-10 13:48:25 +0200

    parsing done

--- parsing.c
+++ parsing.c
@@ -16,25 +16,113 @@
 	// 	`number_of_coders time_to_burnout time_to_compile time_to_debug 
 	// time_to_refactor number_of_compiles_required dongle_cooldown scheduler`
 	// check if number
-	// if negative
+	// if negative no 
 	// if longer then int should fail
 	
+#include <stdio.h>
 
-
-int initial_checks(int *args)
+size_t	ft_strlen(const char *s)
 {
-	if (!*args)
-		return 1;
-	else if (*args != 8)
-		return 1;
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int ft_strncmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else 
+			return (0);
+	}
+	if (!s1[i])
+		return (1);
+	return (0);  
+}
+
+int check_priority_order(char *args)
+{
+	int i;
+	i = 0;
+	if (ft_strlen(args) != 3 && ft_strlen(args) != 4)
+		return (0);
+	if (ft_strlen(args) == 3)
+	{
+		if (!ft_strncmp(args, "edf"))
+			return (0);
+	}
+	else if (ft_strlen(args) == 4)
+	{
+		if (!ft_strncmp(args, "fifo"))
+			return (0);
+	}
+	return (1);
 }
 
 
-int formating(int *args)
+int is_valid_number(char *args)
 {
-	int i = 0;
-	while(args[i])
+	int i;
+	
+	i = 0;
+	while(args[i] == ' ' || args[i] == '\t')
+		i++;
+	if (args[i] == '\0')
+		return (0);
+	if (args[i] == '+')
+		i++;
+	if (args[i] == '-')
+		return (0);
+	if (args[i] < '0' || args[i] > '9')
+		return (0);
+	while (args[i])
 	{
-		if (args[i] )
-	}	
+		if (args[i] < '0' || args[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	only_int_allowed(char **str)
+{
+	int	i;
+
+	i = 1;
+	while (i < 8)
+	{
+		if (!is_valid_number(str[i]))
+			return (0);
+		i++;
+	}
+	if(!check_priority_order(str[i]))
+		return (0);
+
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 9)
+	{
+		printf("You need to provide 7 int + type of priority check (e.g. fifo or edf)\n");
+		return (1);
+	}
+
+	if (!only_int_allowed(argv))
+	{
+		printf("Invalid arguments\n");
+		return (1);
+	}
+	else
+		printf("The program is working and is now going to start");
+	return (0);
 }
