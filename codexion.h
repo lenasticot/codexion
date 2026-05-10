9c5de4aa67bbbc1fb78d8e4390f606e814637bcd
lena <lena.odum1@gmail.com>
2026-05-10 21:36:41 +0200

    working on struct

--- codexion.h
+++ codexion.h
@@ -1,5 +1,7 @@
+
 #ifndef CODEXION_H
 # define FT_PRINTF_H
+
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -11,6 +13,42 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+
+// What do i need my coder to be
+// need to learn about the heap
+
+
+// need to work also on time and managing milliseconds
+
+typedef struct coder {
+	// Just the number of the coder but to see if this is interesting or not
+	int nb;
+	// maybe need to do a hierachy between them especially to know which one has the priority
+	int rank;
+	// for status could be equivalent to compile debug and refactor
+	// and to be equal to 0 1 or 2
+	int status;
+	// or they have individual debug, refac and compile
+	// 0 when off
+	// 1 when on
+	// always need to check if the other is on 
+	// need to see what could be the most efficient
+	int debug;
+	int refac;
+	int comp;
+	int burnout_time;
+	int cooldown;
+	// to know how many time the coder has compiled already
+	// to be decreased until 0
+	int compiled;
+	// to check how many dongle they have in their hand
+	int dongle;
+
+}
+
+struct dongle {
+	int rank;
+}
 
 
 
